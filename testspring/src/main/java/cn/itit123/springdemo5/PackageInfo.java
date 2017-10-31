97157d5658ca980a58f68ab5bebbc3a56316a49c
zhangguozhao <412687009@qq.com>
2017-10-31 16:21:14 +0800

    Revert "Revert "testspring:Srping4事务管理""
    This reverts commit a8fd44577557c3f55b7d97b99135214184e15f8c.

---
+++ testspring/src/main/java/cn/itit123/springdemo5/PackageInfo.java
@@ -0,0 +1,9 @@
+package cn.itit123.springdemo5;
+
+/** 本包介绍spring4  事务管理
+ * 根据网上教程所做，原文地址如下
+ * http://blog.csdn.net/qq_19558705/article/details/50681077
+ * Created by ZhangGuozhao on 2017/10/31.
+ */
+public class PackageInfo {
+}
